ec4572d555a7c074fe6d8878231bff5d3e0aea56
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-06 18:32:38 -0800

    working on profile page. updated app

--- App/Client/Views/signinController.js
+++ App/Client/Views/signinController.js
@@ -2,7 +2,10 @@
 import template from './signinTemplate.vue';
 
 const Login = {
+<<<<<<< HEAD
   name: 'Login',
+=======
+>>>>>>> working on profile page. updated app
   template: template.template,
   data () {
     return {
@@ -11,16 +14,23 @@ const Login = {
     }; 
   },
   methods: {
+<<<<<<< HEAD
     // notify: function() {
     //   this.$dispatch('signin-user', this.username);
     // },  
     login: function() {
       // this.notify();
+=======
+    login: function() {
+
+<<<<<<< HEAD
+>>>>>>> working on profile page. updated app
       this.$http.get('/api/user', {
         params: {
           username: this.username,
           password: this.password
         }, 
+<<<<<<< HEAD
       })
       .then((res) => { 
         console.log(this.username);
@@ -28,8 +38,23 @@ const Login = {
       })
       .catch((err) => console.error(err));
     }
+=======
+=======
+      this.$http.get('/api/user', {params: {username: this.username}})
+      .then((res) => {
+        this.$router.push('/profile')
+
+>>>>>>> working on profile page. updated app
+      })
+      .then((res) => { console.log('request sent'); })
+      .catch((err) => console.error(err));
+    },
+>>>>>>> working on profile page. updated app
   }
 };
 
 export default Login;
+<<<<<<< HEAD
  
+=======
+>>>>>>> working on profile page. updated app
