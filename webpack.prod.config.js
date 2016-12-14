0d7cd54564796ced1342214948c25d2a2fb270cf
Nim G <nimrodgrinvald+github@gmail.com>
2016-12-14 15:59:17 -0800

    Prevent production environment from calling .env file (config vars are built into Heroku) by creating a deploy script to work with a webpack production file

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,6 @@
 var webpackUglifyJsPlugin = require('webpack-uglify-js-plugin');
 var path = require('path'); 
 const webpack = require('webpack');
-const DotenvPlugin = require('webpack-dotenv-plugin');
 
 module.exports = {
   entry: './App/Client/app.js',
@@ -34,14 +33,10 @@ module.exports = {
     }
   },
   plugins: [
-    new DotenvPlugin({
-      sample: './.env.example',
-      path: './.env'
-    }),
     new webpack.EnvironmentPlugin([
       'PUBNUB_PUBLISH_KEY',
       'PUBNUB_SUBSCRIBE_KEY'
-    ]),
+    ])/*,
     new webpackUglifyJsPlugin({
       cacheFolder: path.resolve(__dirname, 'public/cached_uglify/'),
       debug: true,
@@ -55,6 +50,6 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    })*/
   ]
 };
