b914545cea659df2c695f20d795fca54c12177dc
Nim G <nimrodgrinvald+github@gmail.com>
2016-12-14 16:18:42 -0800

    Test deployment bugfix

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,19 +1,8 @@
-var webpackUglifyJsPlugin = require('webpack-uglify-js-plugin');
 var path = require('path'); 
 const webpack = require('webpack');
 
 module.exports = {
   entry: './App/Client/app.js',
-  output: {
-    path: './compiled/transpiled',
-    publicPath: 'compiled/transpiled',
-    filename: '[name].js'
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
   module: {
     loaders: [
       {
@@ -27,29 +16,25 @@ module.exports = {
       }
     ]
   },
-  vue: {
-    loaders: {
-      js: 'babel'
-    }
+  output: {
+    path: './compiled/transpiled',
+    publicPath: 'compiled/transpiled',
+    filename: '[name].js'
   },
   plugins: [
     new webpack.EnvironmentPlugin([
       'PUBNUB_PUBLISH_KEY',
       'PUBNUB_SUBSCRIBE_KEY'
-    ])/*,
-    new webpackUglifyJsPlugin({
-      cacheFolder: path.resolve(__dirname, 'public/cached_uglify/'),
-      debug: true,
-      minimize: true,
-      sourceMap: false,
-      output: {
-        path: path.join(__dirname, 'compiled/transpiled'),
-        filename: 'main.js',
-        publicPath: '/public/'
-      },
-      compressor: {
-        warnings: false
-      }
-    })*/
-  ]
+    ])
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
+  }
 };
